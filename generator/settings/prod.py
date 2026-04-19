aa050831260862c7845c855661121a570e5afa8a
aylihC1 <anton.chilievich1@gmail.com>
2026-04-19 15:08:47 +0300

    prod.by changes

--- generator/settings/prod.py
+++ generator/settings/prod.py
@@ -1,5 +1,11 @@
 from .base import *
+import os
+import dj_database_url
 
 DEBUG = False
-
 ALLOWED_HOSTS = ['*']
+
+# Подключаем PostgreSQL из переменной окружения
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), ssl_require=True)
+}
